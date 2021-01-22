a4054d411f8421d88e236afc82f4240a2e166910
Trevor Bonomi <trevorbonomi@gmail.com>
2021-01-22 02:41:58 +0100

    fix: statically

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,6 +3,6 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/asyndesis/asyndesis.github.io/Tales-of-Destiny-DC/"
+    ? "https://cdn.statically.io/gh/asyndesis/Tales-of-Destiny-DC/gh-pages/"
     : "",
 };
