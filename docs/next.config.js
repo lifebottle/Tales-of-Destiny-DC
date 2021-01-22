c8db965a0a727e267758b0a98aaa11f5726ef2fd
Trevor Bonomi <trevorbonomi@gmail.com>
2021-01-22 18:48:47 +0100

    fix: remove asset prefix

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,11 +2,4 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/" +
-      process.env.NEXT_PUBLIC_GITHUB_USER_NAME +
-      process.env.NEXT_PUBLIC_BASE_PATH +
-      "/gh-pages/"
-    : "",
 };
