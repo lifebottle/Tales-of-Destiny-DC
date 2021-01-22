f263cd6472d8002b369d21d0e859403e41283743
Trevor Bonomi <trevorbonomi@gmail.com>
2021-01-22 02:48:16 +0100

    fix: basepath

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,8 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
+const REPO_NAME = "Tales-of-Destiny-DC"; //TODO: Extract this to .env
 
 module.exports = {
+  basePath: "/" + REPO_NAME,
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/asyndesis/Tales-of-Destiny-DC/gh-pages/"
+    ? "https://cdn.statically.io/gh/asyndesis/" + REPO_NAME + "/gh-pages/"
     : "",
 };
