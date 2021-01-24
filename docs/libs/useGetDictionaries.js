d92358e59940ff626aa35b46bbecbb87faadea8c
Trevor Bonomi <trevorbonomi@gmail.com>
2021-01-24 19:58:22 +0100

    fix: serverside

--- docs/libs/useGetDictionaries.js
+++ docs/libs/useGetDictionaries.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useMemo, useState } from "react";
 export const useGetDictionaries = (files = []) => {
   // TODO: Need a better way to get data serverside instead of just cancelling serverside
-  //if (typeof window !== "undefined") return false;
+  if (typeof window !== "undefined") return false;
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   useMemo(async () => {
