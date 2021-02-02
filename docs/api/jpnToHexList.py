eba6228e88bb28311d9ebd9dce00577294406000
Trevor Bonomi <trevorbonomi@gmail.com>
2021-02-02 21:32:58 +0100

    feat: hex to japanese with list ability

---
+++ docs/api/jpnToHexList.py
@@ -0,0 +1,26 @@
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+from urllib.parse import urlparse
+import json
+from .scripts.JpnToHexList import jpnToHexList
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_len = int(self.headers['content-length'])
+        post_body = self.rfile.read(content_len)
+        data = json.loads(post_body)
+        parsed_path = urlparse(self.path)
+        converted = jpnToHexList(data['input'])
+        print(converted)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps({
+            'method': self.command,
+            'path': self.path,
+            'real_path': parsed_path.query,
+            'query': parsed_path.query,
+            'request_version': self.request_version,
+            'protocol_version': self.protocol_version,
+            'body': converted}).encode())
+        return
