0ca594fef05cc3af0e77144006353dc1389ba8e2
pnvnd <peter@datacrunch.ca>
2021-02-22 23:02:35 -0500

    Update dump.py

--- backup/dump.py
+++ backup/dump.py
@@ -6,6 +6,6 @@ Simply inserting binary data and creating ISO will crash the game.
 with open("C:\\temp\\SLPS_258.42", "rb") as file_in:
     with open("C:\\temp\\SLPS_258.42_patched", "wb") as file_out:
         for line in file_in:
-            file_out.write(line.replace(b'Final Demon Fang', b'Infernal Demon Fang'))
+            file_out.write(line.replace(b'\x99\x68\x99\x70\x99\x6D\x99\x65', b'\x47\x4F\x4C\x44\x00\x00\x00\x00'))
 file_in.close()
 file_out.close()
