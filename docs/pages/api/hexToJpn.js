b985a25c670fabb885d3828bd31becd854acef8c
Trevor Bonomi <trevorbonomi@gmail.com>
2021-03-02 20:50:56 +0100

    fix: symlinks

--- docs/pages/api/hexToJpn.js
+++ docs/pages/api/hexToJpn.js
@@ -1,6 +1,6 @@
 import { runPythonScript } from "components/runPythonScript";
 const handler = async (req, res) => {
-  const results = await runPythonScript("api/_scripts/HexToJpn.py", [
+  const results = await runPythonScript("scripts/HexToJpn.py", [
     req?.body?.input,
   ]);
   res.send(results);
