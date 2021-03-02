e78dfb2b7117d40925e7a17407b8156bb1b62164
Trevor Bonomi <trevorbonomi@gmail.com>
2021-03-02 22:09:09 +0100

    Fix script path for python files

--- docs/components/runPythonScript.js
+++ docs/components/runPythonScript.js
@@ -14,7 +14,7 @@ const runPythonScript = (pythonFile, args) => {
     mode: "text",
     pythonPath: process.env.NEXT_PUBLIC_PYTHON_PATH || null,
     pythonOptions: [],
-    //scriptPath: scriptPath,
+    scriptPath: process.env.NEXT_PUBLIC_PYTHON_SCRIPT_PATH || null,
     args: args,
   };
 
