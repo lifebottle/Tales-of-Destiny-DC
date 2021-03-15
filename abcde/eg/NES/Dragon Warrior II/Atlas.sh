b268fdd7e5b8be017c3b12e4a69437ef59dba6a9
fortiersteven <fortiersteven1@gmail.com>
2021-03-15 17:00:16 -0400

    Remove abcde folder

--- abcde/eg/NES/Dragon Warrior II/Atlas.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp "Dragon Warrior II (U) [!].nes" Atlas.nes 2> /dev/null;
-if [ "$?" != "0" ]; then
-	echo "ROM file \"Dragon Warrior II (U) [!].nes\" not found; creating an empty file instead.";
-	cat /dev/null > Atlas.nes;
-fi
-
-perl ../../../abcde.pl -cm abcde::Atlas Atlas.nes Atlas.txt
