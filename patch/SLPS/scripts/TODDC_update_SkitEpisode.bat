eda0e464296541cb1af2859731572577051a41e2
fortiersteven <fortiersteven1@gmail.com>
2021-05-15 18:54:11 -0400

    SkitName + Rework SLPS

--- patch/SLPS/scripts/TODDC_update_SkitEpisode.bat
+++
@@ -1,2 +0,0 @@
-python "code/updateBlock.py" 2 SLPS_Chapter.42
-pause
---
+++ patch/SLPS/scripts/code/abcde/!ToR_abcde_Dump_02.Artes.bat
@@ -0,0 +1,2 @@
+perl abcde.pl -m bin2text -cm abcde::Cartographer "SLPS_254.50_original" "ToR_abcde_Script_02.Artes.txt" ToR_abcde_Dump_02.Artes -s
+pause
