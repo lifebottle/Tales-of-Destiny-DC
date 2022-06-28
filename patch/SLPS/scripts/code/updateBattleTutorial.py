46d268ab0822e7ab76c1cc99911d1a02c7ab104a
Stewie <fortiersteven1@gmail.com>
2022-06-28 17:37:06 -0400

    Update sections to have Battle tutorial

---
+++ patch/SLPS/scripts/code/updateBattleTutorial.py
@@ -0,0 +1,28 @@
+from HelperfunctionsNew import *
+import sys
+import os
+
+if __name__ == "__main__":
+    
+    blockDesc = sys.argv[1]
+    
+    
+    helper = Helper()
+  
+    if blockDesc in ["Skit Name", "Synopsis", "Minigame"]:
+        helper.createBlock_Multi(blockDesc)
+        
+    elif blockDesc != "All":
+    
+
+        
+        print("Create the script based on google sheet")
+        helper.createAtlasScript_Block(blockDesc)
+        
+
+    else:
+        
+        helper.createAtlasScript_All()
+        
+        print("Create the SLPS for this block")
+        helper.reinsertText_All(blockDesc)
