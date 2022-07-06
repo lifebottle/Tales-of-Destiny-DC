3d1db29b447eba0268c3fdf292d4ee39e5eae945
FuriousG4m3r <77897370+furiousg4m3r@users.noreply.github.com>
2022-07-05 22:45:31 -0400

    Update Run_All.ps1

--- patch/SLPS/scripts/Run_All.ps1
+++ patch/SLPS/scripts/Run_All.ps1
@@ -1,4 +1,4 @@
 ﻿python "code/updateBlock.py" All
 python "code/updateBlock.py" "Battle Tutorial"
-python "code/updateBlock.py" "Minigame"
+#python "code/updateBlock.py" "Minigame"
 python "code/updateBlock.py" "Skit Name"
